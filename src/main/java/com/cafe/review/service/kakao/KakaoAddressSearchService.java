5850d62ca5985bd379f8dc9a4fe5ef563a2e5ac4
InKyuHwang001 <hwanginkyu42@naver.com>
2024-05-30 16:14:59 +0900

    feat: 주소 기반 좌표 찾기 기능 개발

--- src/main/java/com/cafe/review/service/kakao/KakaoAddressSearchService.java
+++ src/main/java/com/cafe/review/service/kakao/KakaoAddressSearchService.java
@@ -3,15 +3,36 @@ package com.cafe.review.service.kakao;
 import com.cafe.review.dto.kakao.AddressResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class KakaoAddressSearchService {
 
+    private final RestTemplate restTemplate;
+    private final KaKaoUriBuilderService kaKaoUriBuilderService;
+
+    @Value("${kakao.rest.api.key}")
+    private String kakaoRestApiKey;
+
     public AddressResponseDto requestAddressSearch(String address) {
 
-        return null;
+
+        URI uri = kaKaoUriBuilderService.buildUriByAddressSearch(address);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "KakaoAK " + kakaoRestApiKey);
+        HttpEntity httpEntity = new HttpEntity<>(headers);
+
+        //kakao api 호출
+        return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, AddressResponseDto.class).getBody();
     }
 }
