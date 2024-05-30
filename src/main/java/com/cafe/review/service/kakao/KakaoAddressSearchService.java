9fb2da626868b74994f33ca349595c54230b3954
InKyuHwang001 <hwanginkyu42@naver.com>
2024-05-30 16:03:58 +0900

    test: 주소 좌표 찾기 테스트코드 작성

---
+++ src/main/java/com/cafe/review/service/kakao/KakaoAddressSearchService.java
@@ -0,0 +1,17 @@
+package com.cafe.review.service.kakao;
+
+import com.cafe.review.dto.kakao.AddressResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KakaoAddressSearchService {
+
+    public AddressResponseDto requestAddressSearch(String address) {
+
+        return null;
+    }
+}
---
+++ src/test/java/com/cafe/review/service/kakao/KakaoAddressSearchServiceTest.java
@@ -0,0 +1,31 @@
+package com.cafe.review.service.kakao;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("비즈니스 로직 - KakaoAddressSearchService")
+@SpringBootTest
+class KakaoAddressSearchServiceTest {
+
+    @Autowired
+    KakaoAddressSearchService kakaoAddressSearchService;
+
+    @Test
+    @DisplayName("[requestAddressSearch][success]")
+    void if_address_given_ (){
+        //given
+        String address = "경기도 군포시";
+        //when
+        var result = kakaoAddressSearchService.requestAddressSearch(address);
+
+        //then
+        assertFalse(result.getDocumentList().isEmpty());
+        assertTrue(result.getMetaDto().getTotalCount() > 0);
+        assertNotNull(result.getDocumentList().get(0).getAddressName());
+
+    }
+}
