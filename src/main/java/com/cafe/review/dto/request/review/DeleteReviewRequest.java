c4ba681e395a27a304c0b56202a047dd81743fd8
InKyuHwang001 <hwanginkyu42@naver.com>
2024-06-10 12:07:32 +0900

    feat: validation을 request에 추가

--- src/main/java/com/cafe/review/dto/request/review/DeleteReviewRequest.java
+++ src/main/java/com/cafe/review/dto/request/review/DeleteReviewRequest.java
@@ -1,13 +1,16 @@
 package com.cafe.review.dto.request.review;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @NoArgsConstructor
 public class DeleteReviewRequest {
 
+    @Length(min = 5, max = 15, message = "비밀번호는 5 ~ 15자 입니다.")
     private String password;
 
     @Builder
