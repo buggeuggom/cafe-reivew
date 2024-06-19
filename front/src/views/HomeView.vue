442e7898baf7ee10a72966c293747a5eaf2dd4b0
InKyuHwang001 <hwanginkyu42@naver.com>
2024-06-19 18:06:30 +0900

    feat: link 추가

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,10 +1,24 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-    http method
-    post
+  <div class="demo-collapse">
+    <el-collapse v-model="activeName" accordion>
+      <el-collapse-item title="http method" name="1">
+        <a href="https://docs.google.com/spreadsheets/d/1DFhouNXJJzhR5s1qPbkk59ifF7rd1EZz4iRqePg4CUE/edit?usp=sharing">
+          google doc
+        </a>
+      </el-collapse-item>
 
-  </main>
+      <el-collapse-item title="github" name="2">
+        <a href="https://github.com/InKyuHwang001/cafe-reivew">
+          github Link
+        </a>
+      </el-collapse-item>
+
+    </el-collapse>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeName = ref('1')
+</script>
